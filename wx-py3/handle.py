aa5b8dbcd5f2f11088c8724ae3b39b28546a5ac0
Angus <1621xjx@buaa.edu.cn>
2018-07-09 09:40:14 +0800

    Update wx-py3

--- wx-py3/handle.py
+++ wx-py3/handle.py
@@ -4,13 +4,16 @@ import hashlib
 import reply
 import receive
 import web
-import urllib.request
 import ssl
+import urllib.request
+import urllib.parse
+import re
 class Handle(object):
     def POST(self):
         try:
             webData = web.data()
-            print("Handle Post webdata is ", webData)  # 后台打日志
+            print ("Handle Post webdata is ", webData)
+   #后台打日志
             recMsg = receive.parse_xml(webData)
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
@@ -20,11 +23,31 @@ class Handle(object):
                 output = urllib.request.urlopen(
                     "https://tagging.aminer.cn/query/" + input,
                     context=context).read().decode(encoding='utf-8')
-                print(type(output))
-                replyMsg = reply.TextMsg(toUser, fromUser, output)
+                print(output)
+                dic = re.findall("\"([^\"]*)\": \"([^\"]*)\"", output)
+                url = "https://www.aminer.cn/search?"
+                ans = ""
+                for info in dic:
+                    if info[1] == "org":
+                        url += "o=" + urllib.parse.quote(info[0]) + "&"
+                        ans += "来自" + info[0]
+                for info in dic:
+                    if info[1] == "name":
+                        url += "n=" + urllib.parse.quote(info[0]) + "&"
+                        ans += "名为" + info[0] + "的学者"
+                for info in dic:
+                    if info[1] == "item":
+                        url += "k=" + urllib.parse.quote(info[0]) + "&"
+                        ans += "的" + info[0]
+                url = url[:-1]
+                link = "<a href=\"" + url + "\">" + ans + "</a>"
+                print(link)
+                replyMsg = reply.TextMsg(toUser, fromUser, link)
                 return replyMsg.send()
             else:
-                print("暂且不处理")
+                print ("暂且不处理")
                 return "success"
         except Exception as Argment:
             return Argment
+
+
